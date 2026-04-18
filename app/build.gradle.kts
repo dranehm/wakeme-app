e8b1d4c02ed48e5346251a0ac6696be0f205fbc9
dranehm <mdgalapate@gmail.com>
2026-04-18 15:37:58 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
@@ -52,6 +51,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.appwidget:appwidget:1.0.0-alpha03")
 }
 
